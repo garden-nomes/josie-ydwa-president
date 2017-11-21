49b50cb59015bbabb7187edfd37cf82fdd66b5ed
noaner <noahbweiner@gmail.com>
2017-11-20 17:23:09 -0800

    add profile pic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import styles from './index.module.css';
-import profile from '../content/profile.jpg';
+import profile from '../content/profile.png';
 import about from '../content/about.md';
 
 const IndexPage = () => (
