fa1171161a237d71685568f126467f8f36ae6539
noaner <noahbweiner@gmail.com>
2017-11-21 14:40:26 -0800

    Change "YDWA" to "Young Democrats"

--- src/components/EndorseForm.js
+++ src/components/EndorseForm.js
@@ -63,7 +63,7 @@ class EndorseForm extends Component {
 
         <p>
           <label for="title">
-            Former titles with the YDWA (if comfortable/applicable)
+            Former titles with the Young Democrats (if comfortable/applicable)
           </label>
           <input type="text" name="title" onChange={this.handleChange} />
         </p>
